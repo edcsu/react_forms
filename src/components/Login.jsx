8c520790fb6a764db8ebfef5fe242ddfae15c68b
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-14 21:23:49 +0300

    feat: usestate and useref for handling forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: ''})
-
-  function handleInputChange(identifier, value) {
-    setEnteredValues(prevValues => ({
-      ...prevValues,
-      [identifier]: value
-    }))
-  }
+  const email = useRef(second)
+  const password = useRef(second)
 
   function handleSubmit(event) {
      event.preventDefault()
-     
+
+     const enteredEmail = email.current.value
+     const enteredPassword = password.current.value
   }
 
   return (
@@ -23,24 +17,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input 
-            id="email" 
-            type="email" 
-            name="email" 
-            onChange={(event) =>handleInputChange('email', event.target.value)} 
-            value={enteredValues.email} 
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-           id="password" 
-           type="password" 
-           name="password" 
-           onChange={(event) =>handleInputChange('password', event.target.value)} 
-           value={enteredValues.password} 
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
